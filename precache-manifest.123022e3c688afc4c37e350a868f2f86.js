d0c0023e96693f8f943fdad5c2929c04de347878
KosBelozyorov <KosBelozyorov@gmail.com>
2019-09-13 16:56:56 +0300

    Updates

---
+++ precache-manifest.123022e3c688afc4c37e350a868f2f86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5f73b38fec805a0c565c2002d67aa2",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "1ba6a49ce33ef5060c54",
+    "url": "/react_tabs/static/css/main.2ff72be0.chunk.css"
+  },
+  {
+    "revision": "2a2dcc1a871e22b7b964",
+    "url": "/react_tabs/static/js/2.75c4c41f.chunk.js"
+  },
+  {
+    "revision": "1ba6a49ce33ef5060c54",
+    "url": "/react_tabs/static/js/main.b11ef5e4.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
